b5f52e0245c513f9b96e22338b8bbbe31542d94f
Chelsam43 <clmortenson1@dmacc.edu>
2026-01-20 23:38:57 -0600

    futurevaluemortenson

---
+++ Ch02FutureVAlueMortenson/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ch02FutureVAlueMortenson;
+using Ch02FutureVAlueMortenson.Models;
+
+namespace Ch02FutureVAlueMortenson.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            
+            ViewBag.FV = 0;
+            return View();
+        }
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.Fv = 0;
+            }
+                return View(model);
+        }
+
+    }
+}
