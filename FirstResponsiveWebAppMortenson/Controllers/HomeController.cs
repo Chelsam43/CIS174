d58920de7749e1a51073b21b2a1be90a1ad7c20b
Chelsam43 <clmortenson1@dmacc.edu>
2026-01-21 20:05:45 -0600

    FirstResponsiveWebApp

---
+++ FirstResponsiveWebAppMortenson/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Name: Chelsa Mortenson
+ * Date: 01/20/2026
+ * CIS 174
+ * OS: Windows 11 home
+ * Visual Studio 2022
+ * Assignment:
+ * Write a Responsive Web App named FirstResponsiveWebAppLastName and does the following:
+    Has a label and input for a user's name
+    Has a label and input for year of birth
+    Has a submit and clear button
+    Model
+    Attributes: Name, BirthYear
+    Method: AgeThisYear()
+    No Magic Numbers (HINT: Use a global constant) 
+    Validate user input
+    On submit, displays their name and what their age will be Dec 31st of this year. 
+    Extra credit: Get user's birthday and display their name and what age they are today. (2 points extra)
+*Discription: ASP.NET COre MVC app that the user inputs name nad the year of birth, the app validates the input, and outputs a resut that has the input  name, calculated age based on the BirthYear input minus the current year.
+*Bootstrap is used for responsive design layout and it uses both client and server side validation.
+*/
+
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppMortenson.Models;
+
+
+
+
+namespace FirstResponsiveWebAppMortenson.Controllers
+{
+    //handler for hompage request
+    public class HomeController : Controller
+    {
+        //Shows in input form
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Handle submission and validation of input
+        [HttpPost]
+        public IActionResult Index(UserModel user)
+        {
+
+            //stop/prevention of invalid input
+            if(!ModelState.IsValid)
+            { 
+                
+                return View();
+        }
+
+            //shows valid input result
+            return View("Result", user);
+        
+    }
+
+        //clear form and back to index page
+    public IActionResult Clear()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstResponsiveWebAppMortenson/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿
+//import validation for Model Properies
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppMortenson.Models
+{
+
+    //Model that stores guidlines for user input and validation
+    public class UserModel
+    {
+        //const to calculate age
+        public const int CURRENT_YEAR = 2026;
+
+
+        //user name required, only accepting letters. it will accept spaces
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name can only contain letters")]
+        public string Name { get; set; } = string.Empty;
+
+
+        //user input of birth year required, year has to be 1900 or later. 
+        [Required(ErrorMessage = "Birth year is required")]
+        [Range(1900, CURRENT_YEAR, ErrorMessage = "Birth year must be between 1900 and 2026")]
+        public int BirthYear { get; set; }
+
+        //calculation of user's age, sutracts from current year
+        public int AgeThisYear => CURRENT_YEAR - BirthYear;
+       
+        //current year public to be used in other areas/views
+        public int CurrentYear => CURRENT_YEAR;
+    }
+}
+
